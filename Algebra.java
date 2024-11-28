956d7dd412fe686e74a6047fdd082f825642d2ed
einav <einav.manor@post.runi.ac.il>
2024-11-28 22:09:10 +0200

    upload HW3

--- Algebra.java
+++ Algebra.java
@@ -1,4 +1,4 @@
-// Implements algebraic operations and the square root function without using 
+
 // the Java operations a + b, a - b, a * b, a / b, a % b, and without calling 
 // Math.sqrt. All the functions in this class operate on int values and
 // return int values.
@@ -6,12 +6,12 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
+	    System.out.println(plus(210,100));   // 2 + 3
+	    System.out.println(minus(787,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
+ 		System.out.println(times(-3,-4));  // -3 * -4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
+   		System.out.println(pow(-5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
    		System.out.println(div(12,3));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
@@ -23,45 +23,162 @@ public class Algebra {
    		System.out.println(sqrt(76123));
 	}  
 
-	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int i = x1;
+		int j = 0;
+		while (j != x2) {
+			i++;
+			j++;	
+		}
+		return i;
 	}
 
-	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int i = x1;
+		int j = 0;
+		while (j != x2) {
+			i--;
+			j++;	
+		}
+		return i;
 	}
 
-	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int j = 1;
+		int i = x1;
+		boolean negetive = false;
+		if(x2 == 0 || x1 == 0){
+			i =0;
+			return (i);
+		}
+		if (x2 < 0){
+			int b = minus(0, x2);
+			negetive =true;
+			x2 = b;
+			}
+		if (x1 < 0){
+			x1 = minus(0, x1);
+			if(negetive == true){
+				negetive =false;
+				i = x1;
+			}else{
+				negetive =true;
+				i = x1;
+			}		
+		}
+		int midSum = i;
+		if (x1 >0 && x2 > 0){
+			while (j < x2) {
+				midSum = (plus(i, midSum));	
+				j++;	
+			}
+			i = midSum;
+		}
+		if(negetive == true){
+			i = minus(0, i);
+		}
+	
+	return (i);
 	}
 
 	// Returns x^n (for n >= 0)
-	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+	public static int pow(int x1, int x2) {
+		int i = x1;
+		int n = 1;
+		boolean negetive = false;
+		if(x2 == 0){
+			return 1;
+		}
+		if (x1 <= 0){
+			i = minus(0,i);
+			int mood = (mod(x2,2));
+			if(mood != 0){
+				negetive = true;
+			}	
+		}
+		int midSum = i;
+		while (n < x2){
+			midSum = times(midSum, x1);	
+			n++;
+			i = midSum;
+		}
+		if(negetive == true){
+			i = minus(0,i);
+		}
+	return (i);
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int i = x1;
+		boolean negetive = false;
+		int j = 0;
+		if(x1 == 0){
+			j = 0;
+		}
+		if (x2 < 0){
+			int b = minus(0, x2);
+			negetive = true;
+			x2 = b;
+			}
+		if (x1 < 0){
+			x1 = minus(0, x1);
+			if (negetive == true){
+				negetive =false;
+				i = x1;
+			}else{
+				negetive =true;
+				i = x1;
+			}		
+		}
+		int midSum = i;
+		while (midSum >= x2) {
+			midSum = (minus(midSum, x2));	
+			j++;	
+		}
+		if(negetive == true){
+			j = minus(0, j);
+		}
+	return (j);
 	}
-
-	// Returns x1 % x2
+	
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int i = x1;
+		int midSum = i;
+		if(x1 == 0){
+			return 0;
+			}
+		if(x2 < 0){
+			x2= (minus(0, x2));
+		}
+		if(x1 < 0){
+			x1= (minus(0, x1));
+		}
+		i = x1;
+		midSum = i;
+		while (midSum >= x2) {
+				midSum = (minus(midSum, x2));
+				}
+		return midSum;
 	}	
 
-	// Returns the integer part of sqrt(x) 
-	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+	public static int sqrt(int x){
+		if (x == 0){
+			return x;
+		}
+		int j = 1;
+		int midSum = 0;
+		int top = 0 ;
+		while (top <= x){
+			midSum = (div(x, j));	
+			j++;
+			top = (pow(j,2));
+			}		
+		if(top > x){
+			j--;
+			return j;
+		}else{
+			return j;
+		}
+		}
 	}	  	  
-}
